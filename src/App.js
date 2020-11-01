fa4a0b93386452be7ddbd42688b61a807e6e69f9
Bolshevik35 <hongquan03051997@gmail.com>
2020-11-01 10:33:34 -0800

    register page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import Register from './Components/Register/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+document.title = "Covid-19 Pandemic";
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'main',
+      isSignIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        joined: ''
+      }
+    };
+  }
+
+  UserInfo = (data) => {
+    this.setState({ user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      joined: data.joined
+    }})
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'home'){
+      this.setState({isSignIn: true}); 
+    }
+    else if (route === 'register'){
+      this.setState({isSignIn: false});
+    }
+    else{
+      this.setState({isSignIn: false});      
+    }
+    this.setState({route: route})
+  }
+
+  render(){
+    const {route, isSignIn} = this.state ;
+    return (
+      <div className="App">
+        <NavBar isSignIn={isSignIn} onRouteChange={this.onRouteChange}/>
+        {route === 'main' ?
+          <h1>This is main page</h1>
+          
+          :
+          (route === 'register' ?
+            <Register UserInfo={this.UserInfo} onRouteChange={this.onRouteChange}/> 
+            :
+            <h1>Sign in Page</h1>
+          )
+        }      
+
+      </div>
+    );
+  }
 }
 
 
---
+++ src/Components/Register/Register.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import './Register.css';
+
+class Register extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fullname: '',
+			email: '',
+			password: '',
+			dob: '',
+			city: ''
+		}
+	}
+
+	onNameChange = (event) => {
+		this.setState({fullname: event.target.value})
+	}
+
+	onEmailChange = (event) => {
+		this.setState({email: event.target.value})
+	} 
+
+	onPasswordChange = (event) => {
+		this.setState({password: event.target.value})
+	}
+
+	onDoBChange = (event) => {
+		this.setState({dob: event.target.value})
+	}
+
+	onCityChange = (event) => {
+		this.setState({city: event.target.value})
+	}
+
+	
+
+	render(){
+		const {onRouteChange} = this.props;
+		return(
+			<div className='flex justify-around flex-row mv6'>
+				<div className='dib br3 ma3 w-40'>
+					<h1>Did you know?</h1>
+					<p><strong>Coronavirus disease (COVID-19)</strong> is an infectious 
+							disease caused by a newly discovered coronavirus. </p>
+					<p>Most people infected with the COVID-19 virus will experience 
+						mild to moderate respiratory illness and recover without 
+						requiring special treatment.  Older people, and those with 
+						underlying medical problems like cardiovascular disease, 
+						diabetes, chronic respiratory disease, and cancer are more 
+						likely to develop serious illness.</p>
+					<details>
+  						<summary>Read more!!!</summary>
+  							<a href="https://www.who.int/health-topics/coronavirus#tab=tab_1">WHO CoronaVirus Information</a>
+					</details> 
+				</div>
+				<div className='br3 ba'> 
+					<form>
+						<h1>Register Form</h1>
+						<div>
+					        <label className="fw6 lh-copy f5 ma3" htmlFor="full-name">Fullname:</label>
+							<input className="ma3 w-70" id="fn" type="text" name="fullname" size="50" placeholder="eg: Anderson Parker" 
+								onfocus="this.placeholder = ''"
+								onChange={this.onNameChange}/>
+						</div>
+						<div>
+							<label className="fw6 lh-copy f5 ma3" htmlFor="email-address">Username: </label>
+							<input className="ma3 w-70" id="email" type="email" name="email" size="50" required
+								placeholder="eg: example@email.com" 
+								onfocus="this.placeholder = ''"
+								onChange={this.onEmailChange}/>
+						</div>
+						<div className='flex justify-around flex-row'>
+							<label className="fw6 lh-copy f5 ma3" htmlFor="password">Password: </label>
+							<input className="ma3 w-70" id="password" type="email" name="email" size="50" required
+								placeholder="eg: example@email.com" 
+								onfocus="this.placeholder = ''"
+								onChange={this.onPasswordChange}/>
+						</div>
+						<div>
+							<label className="fw6 lh-copy f5 ma3" htmlFor="dob">Date of Birth: </label>
+							<input className="ma3 w-60" id="dob" type="date" name="dob" size="50" required 
+								onChange={this.onDoBChange}/>
+						</div>
+						<div>
+							<label className="fw6 lh-copy f5 ma3" htmlFor="city">City: </label>
+							<input className="ma3 w-70" id="dob" type="text" name="city" size="50" list="cities" required 
+								onChange={this.onCityChange}/>
+							<datalist id="cities">
+								<option>Vancouver</option>
+								<option>Victoria</option>	
+								<option>Edmonton</option>	
+								<option>Calgary</option>	
+								<option>Regina</option>	
+								<option>Saskatoon</option>	
+								<option>Winnipeg</option>		
+								<option>Toronto</option>		
+								<option>Mississauga</option>		
+								<option>Ottawa</option>		
+								<option>Montreal</option>		
+							</datalist>
+						</div>
+						<div className="mb3">
+				      		<input
+						      	className="b ph3 pv2 input-reset ba b--black bg-transparent dim pointer f6 dib" 
+						      	type="submit" 
+						      	value="Register"/>
+				    	</div>
+				    	<div className="ma3">
+					    	<input
+					    		onClick={() => onRouteChange('main')}
+					      		className="b ph3 pv2 input-reset ba b--black bg-transparent dim pointer f6 dib" 
+					      		type="submit" 
+					      		value="Return to Main Page"/>
+					    </div>
+
+					</form>
+				</div>
+					
+			</div>
+		);
+	}
+}
+
+export default Register;
