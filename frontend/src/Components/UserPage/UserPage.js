db7a2ea2b2c27514a762cd5988767b457e58622d
Nontawat Janpongsri <njanpong2610@gmail.com>
2020-11-07 18:03:21 -0800

    improve diag_form

--- frontend/src/Components/UserPage/UserPage.js
+++ frontend/src/Components/UserPage/UserPage.js
@@ -6,12 +6,21 @@ class UserPage extends React.Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      total: "0",
+	      total: "0", 
 	      fever: "0",
 	      cough: "0",
 	      tired: "0",
 	      sore: "0",
 	      diarrhoea: "0",
+	      aches: "0",
+	      pink_eye: "0",
+	      headache: "0",
+	      no_taste: "0",
+	      no_smell: "0",
+	      rash: "0",
+	      breathing: "0",
+	      chest_pain: "0",
+	      movement: "0",
 	      showModal: false
 
 	    };
@@ -29,16 +38,23 @@ class UserPage extends React.Component {
 
 	SeePercent = (e) => {
 		this.setState({
-			total: parseInt(this.state.fever) + parseInt(this.state.cough)+parseInt(this.state.tired)+parseInt(this.state.sore)+parseInt(this.state.diarrhoea),
-			fever: "0",
-			cough: "0",
-		    tired: "0",
-		    sore: "0",
-		    diarrhoea: "0",
+			total:  parseInt(this.state.fever) + 
+					parseInt(this.state.cough) +
+					parseInt(this.state.tired) + 
+					parseInt(this.state.sore) +
+					parseInt(this.state.diarrhoea) + 
+					parseInt(this.state.aches) +
+					parseInt(this.state.pink_eye) +
+					parseInt(this.state.headache) +
+					parseInt(this.state.no_taste) +
+					parseInt(this.state.no_smell) +
+					parseInt(this.state.rash) +
+					parseInt(this.state.breathing) +
+					parseInt(this.state.chest_pain) +
+					parseInt(this.state.movement)
+
 		}, function () {
-			this.setState({total: (parseInt(this.state.total)/5)*100},function () {
-				window.alert("the percentage of you having COVID is :  " + this.state.total);
-			})	
+			this.setState({total: (parseInt(this.state.total)/23)*100})	
 		})	
 	}
 
@@ -49,7 +65,24 @@ class UserPage extends React.Component {
 	}
 	  
 	handleCloseModal () {
-	    this.setState({ showModal: false });
+	    this.setState({ 
+	    	showModal: false,
+	    	fever: "0",
+			cough: "0",
+		    tired: "0",
+		    sore: "0",
+		    diarrhoea: "0",
+		    aches: "0",
+	      	pink_eye: "0",
+	      	headache: "0",
+	      	no_taste: "0",
+	      	no_smell: "0",
+	      	rash: "0",
+	      	breathing: "0",
+	      	chest_pain: "0",
+	      	movement: "0",
+		    total:0
+	    });
 	}
 
 
@@ -64,31 +97,32 @@ class UserPage extends React.Component {
 				</ul>
 				<div class="page_body">
 					<h1>Check if you have COVID</h1>
-					<button onClick={this.SeePercent} value="5">Check</button>
 					<button onClick={this.handleOpenModal}>Open Form</button>
+					
 			        <ReactModal 
 			           isOpen={this.state.showModal}
 			           contentLabel="diag form"			           
 			        >
 			          <button onClick={this.handleCloseModal}>Cancel</button>
-			          
+			          <button onClick={this.SeePercent}>Check</button>
+			   
 			          <div onChange={this.handleChange}>
 			          	<h3>Do you have a fever</h3>
-						<input type="radio" id="fever_yes" name="fever" value="1"/>
+						<input type="radio" id="fever_yes" name="fever" value="2"/>
 						<label for="fever_yes">Yes</label>
 						<input type="radio" id="fever_no" name="fever" value="0"/>
 						<label for="fever_no">No</label><br/>
 						</div>
 						<div onChange={this.handleChange}>
 							<h3>Do you have a dry cough</h3>
-							<input type="radio" id="cough_yes" name="cough" value="1"/>
+							<input type="radio" id="cough_yes" name="cough" value="2"/>
 							<label for="cough_yes">Yes</label>
 							<input type="radio" id="cough_no" name="cough" value="0"/>
 							<label for="cough_no">No</label><br/>
 						</div>
 						<div onChange={this.handleChange}>
 							<h3>Do you feel tired all the time</h3>
-							<input type="radio" id="tired_yes" name="tired" value="1"/>
+							<input type="radio" id="tired_yes" name="tired" value="2"/>
 							<label for="tired_yes">Yes</label>
 							<input type="radio" id="tired_no" name="tired" value="0"/>
 							<label for="tired_no">No</label><br/>
@@ -107,6 +141,74 @@ class UserPage extends React.Component {
 							<input type="radio" id="diarrhoea_no" name="diarrhoea" value="0"/>
 							<label for="diarrhoea_no">No</label><br/><br/>
 						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have aches and pain</h3>
+							<input type="radio" id="aches_yes" name="aches" value="1"/>
+							<label for="aches_yes">Yes</label>
+							<input type="radio" id="aches_no" name="aches" value="0"/>
+							<label for="aches_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have conjunctivitis (pink_eye)</h3>
+							<input type="radio" id="pink_eye_yes" name="pink_eye" value="1"/>
+							<label for="pink_eye_yes">Yes</label>
+							<input type="radio" id="pink_eye_no" name="pink_eye" value="0"/>
+							<label for="pink_eye_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have headache</h3>
+							<input type="radio" id="headache_yes" name="headache" value="1"/>
+							<label for="headache_yes">Yes</label>
+							<input type="radio" id="headache_no" name="headache" value="0"/>
+							<label for="headache_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have loss of taste</h3>
+							<input type="radio" id="no_taste_yes" name="no_taste" value="1"/>
+							<label for="no_taste_yes">Yes</label>
+							<input type="radio" id="no_taste_no" name="no_taste" value="0"/>
+							<label for="no_taste_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have loss of smell</h3>
+							<input type="radio" id="no_smell_yes" name="no_smell" value="1"/>
+							<label for="no_smell_yes">Yes</label>
+							<input type="radio" id="no_smell_no" name="no_smell" value="0"/>
+							<label for="no_smell_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have a rash on skin, or discolouration of fingers or toes</h3>
+							<input type="radio" id="rash_yes" name="rash" value="1"/>
+							<label for="rash_yes">Yes</label>
+							<input type="radio" id="no_smell_no" name="rash" value="0"/>
+							<label for="rash_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have a difficulty in breathing or shortness of breath</h3>
+							<input type="radio" id="breathing_yes" name="breathing" value="3"/>
+							<label for="breathing_yes">Yes</label>
+							<input type="radio" id="breathing_no" name="breathing" value="0"/>
+							<label for="breathing_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have a chest pain or pressure</h3>
+							<input type="radio" id="chest_pain_yes" name="chest_pain" value="3"/>
+							<label for="chest_pain_yes">Yes</label>
+							<input type="radio" id="breathing_no" name="chest_pain" value="0"/>
+							<label for="chest_pain_no">No</label><br/><br/>
+						</div>
+						<div onChange={this.handleChange}>
+							<h3>Do you have a loss of speech or movement</h3>
+							<input type="radio" id="movement_yes" name="movement" value="3"/>
+							<label for="movement_yes">Yes</label>
+							<input type="radio" id="movement_no" name="movement" value="0"/>
+							<label for="movement_no">No</label><br/><br/>
+						</div>
+						<button onClick={this.handleCloseModal}>Cancel</button>
+			          	<button onClick={this.SeePercent}>Check</button>
+
+
+						<h2>The percentage of you having COVID-19 is {this.state.total}</h2>
 					
 
 			        </ReactModal>
