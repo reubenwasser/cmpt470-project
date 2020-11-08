c1ebdf744766b8c642140d7e940ca71e4b52d509
Nontawat Janpongsri <njanpong2610@gmail.com>
2020-11-08 00:13:32 -0800

    diag form css

--- frontend/src/Components/UserPage/UserPage.js
+++ frontend/src/Components/UserPage/UserPage.js
@@ -100,115 +100,113 @@ class UserPage extends React.Component {
 					<button onClick={this.handleOpenModal}>Open Form</button>
 					
 			        <ReactModal 
-			           isOpen={this.state.showModal}
-			           contentLabel="diag form"			           
+			        	isOpen={this.state.showModal}
+			        	contentLabel="diag form"	
+			        	className = "Modal"	
 			        >
-			          <button onClick={this.handleCloseModal}>Cancel</button>
-			          <button onClick={this.SeePercent}>Check</button>
+			        	<button class="close_form" onClick={this.handleCloseModal}>Cancel</button>
+			        	<button class="check_per" onClick={this.SeePercent}>Check</button>
 			   
-			          <div onChange={this.handleChange}>
-			          	<h3>Do you have a fever</h3>
-						<input type="radio" id="fever_yes" name="fever" value="2"/>
-						<label for="fever_yes">Yes</label>
-						<input type="radio" id="fever_no" name="fever" value="0"/>
-						<label for="fever_no">No</label><br/>
+			        	<div class="input_label" onChange={this.handleChange}>
+				          	<h3>Do you have a fever</h3>
+							<input class="input_yes" type="radio" id="fever_yes" name="fever" value="2"/>
+							<label for="fever_yes">Yes</label>
+							<input class="input_no" type="radio" id="fever_no" name="fever" value="0"/>
+							<label for="fever_no">No</label><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have a dry cough</h3>
-							<input type="radio" id="cough_yes" name="cough" value="2"/>
+							<input class="input_yes" type="radio" id="cough_yes" name="cough" value="2"/>
 							<label for="cough_yes">Yes</label>
-							<input type="radio" id="cough_no" name="cough" value="0"/>
+							<input class="input_no" type="radio" id="cough_no" name="cough" value="0"/>
 							<label for="cough_no">No</label><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you feel tired all the time</h3>
-							<input type="radio" id="tired_yes" name="tired" value="2"/>
+							<input class="input_yes" type="radio" id="tired_yes" name="tired" value="2"/>
 							<label for="tired_yes">Yes</label>
-							<input type="radio" id="tired_no" name="tired" value="0"/>
+							<input class="input_no" type="radio" id="tired_no" name="tired" value="0"/>
 							<label for="tired_no">No</label><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have sore throat</h3>
-							<input type="radio" id="sore_yes" name="sore" value="1"/>
+							<input class="input_yes" type="radio" id="sore_yes" name="sore" value="1"/>
 							<label for="sore_yes">Yes</label>
-							<input type="radio" id="sore_no" name="sore" value="0"/>
+							<input class="input_no" type="radio" id="sore_no" name="sore" value="0"/>
 							<label for="sore_no">No</label><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have diarrhoea</h3>
-							<input type="radio" id="diarrhoea_yes" name="diarrhoea" value="1"/>
+							<input class="input_yes" type="radio" id="diarrhoea_yes" name="diarrhoea" value="1"/>
 							<label for="diarrhoea_yes">Yes</label>
-							<input type="radio" id="diarrhoea_no" name="diarrhoea" value="0"/>
+							<input class="input_no" type="radio" id="diarrhoea_no" name="diarrhoea" value="0"/>
 							<label for="diarrhoea_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have aches and pain</h3>
-							<input type="radio" id="aches_yes" name="aches" value="1"/>
+							<input class="input_yes" type="radio" id="aches_yes" name="aches" value="1"/>
 							<label for="aches_yes">Yes</label>
-							<input type="radio" id="aches_no" name="aches" value="0"/>
+							<input class="input_no" type="radio" id="aches_no" name="aches" value="0"/>
 							<label for="aches_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have conjunctivitis (pink_eye)</h3>
-							<input type="radio" id="pink_eye_yes" name="pink_eye" value="1"/>
+							<input class="input_yes" type="radio" id="pink_eye_yes" name="pink_eye" value="1"/>
 							<label for="pink_eye_yes">Yes</label>
-							<input type="radio" id="pink_eye_no" name="pink_eye" value="0"/>
+							<input class="input_no" type="radio" id="pink_eye_no" name="pink_eye" value="0"/>
 							<label for="pink_eye_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have headache</h3>
-							<input type="radio" id="headache_yes" name="headache" value="1"/>
+							<input class="input_yes" type="radio" id="headache_yes" name="headache" value="1"/>
 							<label for="headache_yes">Yes</label>
-							<input type="radio" id="headache_no" name="headache" value="0"/>
+							<input class="input_no" type="radio" id="headache_no" name="headache" value="0"/>
 							<label for="headache_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have loss of taste</h3>
-							<input type="radio" id="no_taste_yes" name="no_taste" value="1"/>
+							<input class="input_yes" type="radio" id="no_taste_yes" name="no_taste" value="1"/>
 							<label for="no_taste_yes">Yes</label>
-							<input type="radio" id="no_taste_no" name="no_taste" value="0"/>
+							<input class="input_no" type="radio" id="no_taste_no" name="no_taste" value="0"/>
 							<label for="no_taste_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have loss of smell</h3>
-							<input type="radio" id="no_smell_yes" name="no_smell" value="1"/>
+							<input class="input_yes" type="radio" id="no_smell_yes" name="no_smell" value="1"/>
 							<label for="no_smell_yes">Yes</label>
-							<input type="radio" id="no_smell_no" name="no_smell" value="0"/>
+							<input class="input_no" type="radio" id="no_smell_no" name="no_smell" value="0"/>
 							<label for="no_smell_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have a rash on skin, or discolouration of fingers or toes</h3>
-							<input type="radio" id="rash_yes" name="rash" value="1"/>
+							<input class="input_yes" type="radio" id="rash_yes" name="rash" value="1"/>
 							<label for="rash_yes">Yes</label>
-							<input type="radio" id="no_smell_no" name="rash" value="0"/>
+							<input class="input_no" type="radio" id="no_smell_no" name="rash" value="0"/>
 							<label for="rash_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have a difficulty in breathing or shortness of breath</h3>
-							<input type="radio" id="breathing_yes" name="breathing" value="3"/>
+							<input class="input_yes" type="radio" id="breathing_yes" name="breathing" value="3"/>
 							<label for="breathing_yes">Yes</label>
-							<input type="radio" id="breathing_no" name="breathing" value="0"/>
+							<input class="input_no" type="radio" id="breathing_no" name="breathing" value="0"/>
 							<label for="breathing_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have a chest pain or pressure</h3>
-							<input type="radio" id="chest_pain_yes" name="chest_pain" value="3"/>
+							<input class="input_yes" type="radio" id="chest_pain_yes" name="chest_pain" value="3"/>
 							<label for="chest_pain_yes">Yes</label>
-							<input type="radio" id="breathing_no" name="chest_pain" value="0"/>
+							<input class="input_no" type="radio" id="breathing_no" name="chest_pain" value="0"/>
 							<label for="chest_pain_no">No</label><br/><br/>
 						</div>
-						<div onChange={this.handleChange}>
+						<div class="input_label" onChange={this.handleChange}>
 							<h3>Do you have a loss of speech or movement</h3>
-							<input type="radio" id="movement_yes" name="movement" value="3"/>
+							<input class="input_yes" type="radio" id="movement_yes" name="movement" value="3"/>
 							<label for="movement_yes">Yes</label>
-							<input type="radio" id="movement_no" name="movement" value="0"/>
+							<input class="input_no" type="radio" id="movement_no" name="movement" value="0"/>
 							<label for="movement_no">No</label><br/><br/>
 						</div>
-						<button onClick={this.handleCloseModal}>Cancel</button>
-			          	<button onClick={this.SeePercent}>Check</button>
-
-
-						<h2>The percentage of you having COVID-19 is {this.state.total}</h2>
+					
+						<h2 class="show_per">The percentage of you having COVID-19 is {this.state.total}</h2>
 					
 
 			        </ReactModal>
