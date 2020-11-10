ddfdb39c76509e0b1fcfea2b9e9063d025b6fc47
Reuben Wasser <reubenwasser13@gmail.com>
2020-11-09 16:39:08 -0800

    basic leaflet map

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from '@react-google-maps/api';
-import _ from 'lodash';
+import React, { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
+import L from 'leaflet'
+import Locate from "leaflet.locatecontrol";
+//import { Icon } from "leaflet";
 
-import mapStyles from './mapStyles';
+//import mapStyles from './mapStyles';
 import './Map.css';
 
+/*
 import Search from './Search/Search';
 import Establishment from './Establishment/Establishment';
 import Locate from './Locate/Locate';
@@ -26,104 +24,40 @@ const mapContainerStyle = {
 
 const defaultZoom = 8;
 
-// Set to vancouver
-const center = {
-  lat: 49.28273,
-  lng: -123.120735,
-};
-
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
 };
+*/
+
+//Vancouver coordinates
+const position = [49.28273, -123.120735]
+const viewLevel = 11
+
 
 export default function Map() {
-  const mapRef = React.useRef();
-  const [marker, setMarker] = React.useState(null);
-  const [selected, setSelected] = React.useState(null);
-
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
-
-  const panTo = React.useCallback(({ lat, lng }) => {
-    mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(16);
-  }, []);
-
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-
-  // Add markers to the map based on the search results
-  const handleMarkers = ({
-    lat,
-    lng,
-    name,
-    types,
-    url,
-    formatted_address,
-    open_now,
-  }) => {
-    setMarker({
-      lat: lat,
-      lng: lng,
-      name: name,
-      type: types[0],
-      url: url,
-      formatted_address: formatted_address,
-      open_now: open_now,
-    });
-  };
-
-  const handleCloseClick = () => {
-    setSelected(null);
-  };
-
-  if (loadError) return 'Error Loading Map';
-  if (!isLoaded) return 'Loading Map';
 
   return (
-    <div>
-				<ul class="nav">
-				  <li class="navHome"><a href="/">Home</a></li>
-				  <li class="navStat"><a href="Stats">Stats</a></li>
-          <li class="navMap"><a class="active" href="Map">Map</a></li>
-				  <li class="navReg"><a href="Register" on>Register</a></li>
-				  <li class="navSign"><a href="SignIn">Sign-in</a></li>
-				</ul>
-      <Search panTo={panTo} handleMarkers={handleMarkers} />
-      <Locate panTo={panTo} />
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={defaultZoom}
-        center={center}
-        options={options}
-        onLoad={onMapLoad}
-      >
-        {/* MARKER */}
-        {!_.isEmpty(marker) && (
-          <Marker
-            key={`${marker.lat}-${marker.lng}`}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            onClick={() => {
-              setSelected(marker);
-            }}
-          />
-        )}
-
-        {/* INFO WINDOW */}
-        {selected ? (
-          <InfoWindow
-            position={{ lat: selected.lat, lng: selected.lng }}
-            onCloseClick={handleCloseClick}
-          >
-            <Establishment selected={selected} />
-          </InfoWindow>
-        ) : null}
-      </GoogleMap>
-    </div>
+  <div>
+    <ul class="nav">
+      <li class="navHome"><a href="/">Home</a></li>
+      <li class="navStat"><a href="Stats">Stats</a></li>
+      <li class="navMap"><a class="active" href="Map">Map</a></li>
+      <li class="navReg"><a href="Register" on>Register</a></li>
+      <li class="navSign"><a href="SignIn">Sign-in</a></li>
+    </ul>
+    <MapContainer center={position} zoom={11} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[49.28273, -123.120735]}>
+      <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+      </Marker>
+    </MapContainer>
+  </div>
   );
 }
